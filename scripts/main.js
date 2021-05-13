7367c2f47b9c6a28ae675d79a06a4ed07ea78b63
Felipe Pavan <felipepavang@hotmail.com>
2021-05-13 15:48:17 -0300

    finish js

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+let task = document.getElementById('task-input');
+let addBtn = document.getElementById('btn');
+let list = document.getElementById('task-list');
+
+function addTask() {
+    let taskName = task.value;
+
+    let item = document.createElement('li');
+    item.textContent = taskName;
+    item.setAttribute('id', taskName);
+    list.appendChild(item);
+
+    let removeButton = document.createElement('button');
+    removeButton.setAttribute('id', taskName)
+    removeButton.addEventListener('click', removeTask)
+    removeButton.textContent = 'X'
+    item.appendChild(removeButton);
+}
+
+function removeTask(e) {
+    e.target.parentNode.remove();
+}
+
+addBtn.addEventListener('click', addTask);
